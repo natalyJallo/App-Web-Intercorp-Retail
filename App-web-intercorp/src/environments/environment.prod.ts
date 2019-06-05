313be07e2685b8d74bbc9705195a3e4268f193fc
nataly <marketing20.nat@gmail.com>
2019-06-04 22:47:49 -0500

    se soluciono el problema con el deploy

--- App-web-intercorp/src/environments/environment.prod.ts
+++ App-web-intercorp/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDrZJlasyLE3wIgGpeM6T0z4UlkyaoVHXs",
+    authDomain: "app-web-intercorp-retail.firebaseapp.com",
+    databaseURL: "https://app-web-intercorp-retail.firebaseio.com",
+    projectId: "app-web-intercorp-retail",
+    storageBucket: "app-web-intercorp-retail.appspot.com",
+    messagingSenderId: "602606738138",
+    appId: "1:602606738138:web:b89b748a464d453e"
+  }
 };
